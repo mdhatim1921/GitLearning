0694e17f9f1bde2d752b6699e5f4009569571aed
mdhatim1921 <mdhatim231@gmail.com>
2024-02-02 19:43:03 +0530

    second commit

--- Demo.java
+++ Demo.java
@@ -3,5 +3,9 @@ class Demo
 	public static void main(String [] args)
 	{
 		System.out.println("Hello ");
+	System.out.println("Hello ");
+	System.out.println("Hello ");
+	System.out.println("Hello world ");
 	}
+
 }
